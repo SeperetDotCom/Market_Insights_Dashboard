e178795866c6e8cea9a074fcd54168358ca8e70b
denv3rr <denver.clark@g.austincc.edu>
2025-03-26 21:16:08 -0500

    fix/add sys path for dashboard.py when running on stereamlit

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 import plotly.graph_objs as go
 from core.fetch_data import get_stock_data
